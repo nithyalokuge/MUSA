e08fd5edf44e4bcfcc52602fc3073108b0a41954
nithyalokuge <nithyasamadhilokuge@gmail.com>
2025-07-05 16:39:49 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -62,6 +62,6 @@ app.get("/hunt/games", (req, res) => {
   res.render("hunt/games"); 
 });
 
-app.listen(PORT, () => {
-  console.log(`The server is running at http://localhost:${PORT}!`);
+app.listen(port, () => {
+  console.log(`The server is running at https://localhost:${port}!`);
 });
