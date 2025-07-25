30fdc024c845e209f7ba1b0cebba1d6515a85767
nithyalokuge <nithyasamadhilokuge@gmail.com>
2025-07-25 08:11:15 +0200

    Added db configuration file

---
+++ config/db.js
@@ -0,0 +1,36 @@
+// Database setup
+
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const setupDatabase = async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT
+        });
+
+        // Create database if it doesn't exist
+        await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
+        console.log(`Database '${process.env.DB_NAME}' created successfully!`);
+        await connection.end();
+    } catch (err) {
+        console.error("Failed to connect to the database: ", err.message);
+        throw err;
+    }
+};
+
+setupDatabase();
+
+// Export pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+module.exports = pool;
